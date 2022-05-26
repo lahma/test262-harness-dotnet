ed238b8fe708b7287bc691c031cb8f1e85783eff
Marko Lahma <marko.lahma@gmail.com>
2022-05-26 09:47:59 +0300

    Add NUKE publish steps

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -1,15 +1,29 @@
 using Nuke.Common.CI.GitHubActions;
 
-//[GitHubActions(
-//    "ci",
-//    GitHubActionsImage.WindowsLatest,
-//    GitHubActionsImage.UbuntuLatest,
-//    GitHubActionsImage.MacOsLatest,
-//    //OnPushBranchesIgnore = new[] { MainBranch },
-//    //OnPullRequestBranches = new[] { MainBranch },
-//    PublishArtifacts = false,
-//    InvokedTargets = new[] { nameof(Test), nameof(Pack) },
-//    CacheKeyFiles = new[] { "global.json", "source/**/*.csproj" })]
-partial class Build
+[GitHubActionsAttribute(
+    "pr",
+    GitHubActionsImage.WindowsLatest,
+    GitHubActionsImage.UbuntuLatest,
+    OnPullRequestBranches = new[] { "master", "main" },
+    OnPullRequestIncludePaths = new[] { "**/*.*" },
+    OnPullRequestExcludePaths = new[] { "**/*.md" },
+    PublishArtifacts = false,
+    InvokedTargets = new[] { nameof(Compile), nameof(Test), nameof(Pack) },
+    CacheKeyFiles = new[] { "global.json", "src/**/*.csproj", "src/**/package.json" }),
+]
+[GitHubActionsAttribute(
+    "build",
+    GitHubActionsImage.WindowsLatest,
+    GitHubActionsImage.UbuntuLatest,
+    OnPushBranches = new[] { "master", "main" },
+    OnPushTags = new[] { "v*.*.*" },
+    OnPushIncludePaths = new[] { "**/*.*" },
+    OnPushExcludePaths = new[] { "**/*.md" },
+    PublishArtifacts = true,
+    InvokedTargets = new[] { nameof(Compile), nameof(Test), nameof(Pack), nameof(Publish) },
+    ImportSecrets = new [] { "NUGET_API_KEY", "MYGET_API_KEY" },
+    CacheKeyFiles = new[] { "global.json", "src/**/*.csproj", "src/**/package.json" })
+]
+public partial class Build
 {
 }
--- build/Build.cs
+++ build/Build.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
@@ -29,14 +33,53 @@ partial class Build : NukeBuild
     [GitRepository] readonly GitRepository GitRepository;
     [GitVersion] readonly GitVersion GitVersion;
 
-    const string MainBranch = "main";
-
-    [Parameter("Version")]
-    readonly string Version = "0.0.0";
-
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    static bool IsRunningOnWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+    bool IsTaggedBuild;
+    string VersionPrefix;
+    string VersionSuffix;
+
+    string DetermineVersionPrefix()
+    {
+        var versionPrefix = GitRepository.Tags.SingleOrDefault(x => x.StartsWith("v"))?[1..];
+        if (!string.IsNullOrWhiteSpace(versionPrefix))
+        {
+            IsTaggedBuild = true;
+            Serilog.Log.Information($"Tag version {VersionPrefix} from Git found, using it as version prefix", versionPrefix);
+        }
+        else
+        {
+            var propsDocument = XDocument.Parse(TextTasks.ReadAllText(SourceDirectory / "Directory.Build.props"));
+            versionPrefix = propsDocument.Element("Project").Element("PropertyGroup").Element("VersionPrefix").Value;
+            Serilog.Log.Information("Version prefix {VersionPrefix} read from Directory.Build.props", versionPrefix);
+        }
+
+        return versionPrefix;
+    }
+
+    protected override void OnBuildInitialized()
+    {
+        VersionPrefix = DetermineVersionPrefix();
+
+        VersionSuffix = !IsTaggedBuild
+            ? $"preview-{DateTime.UtcNow:yyyyMMdd-HHmm}"
+            : "";
+
+        if (IsLocalBuild)
+        {
+            VersionSuffix = $"dev-{DateTime.UtcNow:yyyyMMdd-HHmm}";
+        }
+
+        Serilog.Log.Information("BUILD SETUP");
+        Serilog.Log.Information("Configuration:\t{Configuration}", Configuration);
+        Serilog.Log.Information("Version prefix:\t{VersionPrefix}", VersionPrefix);
+        Serilog.Log.Information("Version suffix:\t{VersionSuffix}", VersionSuffix);
+        Serilog.Log.Information("Tagged build:\t{IsTaggedBuild}", IsTaggedBuild);
+    }
 
     Target Clean => _ => _
         .Before(Restore)
