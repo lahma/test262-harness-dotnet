02f18af4ab5bbd7ce0452c4d0b27b9f5ecdee991
Marko Lahma <marko.lahma@gmail.com>
2023-12-28 18:23:05 +0200

    Update to NET 8.0 (#5)
    * upgrade dependencies
    * use artifacts output
    * add package readme

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -4,23 +4,25 @@ using Nuke.Common.CI.GitHubActions;
     "pr",
     GitHubActionsImage.WindowsLatest,
     GitHubActionsImage.UbuntuLatest,
-    OnPullRequestBranches = new[] { "master", "main" },
-    OnPullRequestIncludePaths = new[] { "**/*.*" },
-    OnPullRequestExcludePaths = new[] { "**/*.md" },
+    OnPullRequestBranches = ["master", "main"],
+    OnPullRequestIncludePaths = ["**/*.*"],
+    OnPullRequestExcludePaths = ["**/*.md"],
     PublishArtifacts = false,
-    InvokedTargets = new[] { nameof(Compile), nameof(Test), nameof(Pack) })
+    CacheKeyFiles = new string[0],
+    InvokedTargets = [nameof(Compile), nameof(Test), nameof(Pack)])
 ]
 [GitHubActions(
     "build",
     GitHubActionsImage.WindowsLatest,
     GitHubActionsImage.UbuntuLatest,
-    OnPushBranches = new[] { "master", "main" },
-    OnPushTags = new[] { "v*.*.*" },
-    OnPushIncludePaths = new[] { "**/*.*" },
-    OnPushExcludePaths = new[] { "**/*.md" },
+    OnPushBranches = ["master", "main"],
+    OnPushTags = ["v*.*.*"],
+    OnPushIncludePaths = ["**/*.*"],
+    OnPushExcludePaths = ["**/*.md"],
     PublishArtifacts = true,
-    InvokedTargets = new[] { nameof(Compile), nameof(Test), nameof(Pack), nameof(Publish) },
-    ImportSecrets = new[] { "NUGET_API_KEY", "MYGET_API_KEY" })
+    CacheKeyFiles = new string[0],
+    InvokedTargets = [nameof(Compile), nameof(Test), nameof(Pack), nameof(Publish)],
+    ImportSecrets = ["NUGET_API_KEY", "MYGET_API_KEY"])
 ]
 public partial class Build
 {
